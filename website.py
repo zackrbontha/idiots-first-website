cd6bffb19bb253621c16b0a7ce4f2fb2290ef745
zackrbontha <zbontha@gmail.com>
2018-11-09 04:20:32 -0800

    ASFAWGAWH

--- website.py
+++ website.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,port=port)
